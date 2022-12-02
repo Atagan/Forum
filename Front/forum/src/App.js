0b0ec415bd9ec9deee70b07ea95d488859f60e8f
LauraAtagan <laura.ramos@solera.com>
2022-12-02 12:36:12 +0100

    routing and services

--- Front/forum/src/App.js
+++ Front/forum/src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import MainView from './Components/MainView/MainView';
+import logo from "./logo.svg";
+import "./App.css";
+import MainView from "./Components/MainView/MainView";
 import AddPostButton from "./Components/Thread/AddPostButton";
-import {useState} from 'react';
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  const[buttonPopup, setButtonPopup] = useState(false);
+  const [buttonPopup, setButtonPopup] = useState(false);
+
   return (
-    <div className="App">
-      <MainView></MainView>
-      <AddPostButton trigger={buttonPopup} setTrigger={setButtonPopup}></AddPostButton>
-    </div>
+    <><Router>
+      <Routes>
+        <Route exact path="/home" element={<MainView />} />
+        <Route path="/*" element={<Navigate to="/home" />} />
+      </Routes>
+      </Router></>
   );
 }
 
